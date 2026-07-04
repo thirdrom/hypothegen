fafb93a7ee4f428b63f5880052ae076c01f78771
Team <team@example.com>
2026-07-05 02:42:22 +0300

    del semanticscholar

--- app/tools/__init__.py
+++ app/tools/__init__.py
@@ -5,6 +5,5 @@
 """
 
 from app.tools.paperpilot import search_external
-from app.tools.semscholar import search_external as semscholar_search_external
 
-__all__ = ["search_external", "semscholar_search_external"]
+__all__ = ["search_external"]
